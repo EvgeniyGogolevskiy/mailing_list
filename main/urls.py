edfc7ca9f2a03f708520b2e64776270dc9b1fcc1
EvgeniyGogolevskiy <111510037+EvgeniyGogolevskiy@users.noreply.github.com>
2023-02-06 16:39:31 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
 
-
-from .views import index, tracking
+from .views import index, PixelView
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^static/main/white.jpg', tracking, name='tracking')
+    url(r'^main/white.jpg', PixelView.as_view(), name='tracking')
 ]
